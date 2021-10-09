a2e084eec64a6314220d5a9d109f18cb70b3c093
Andrew Dmitriev <dryundel21@gmail.com>
2021-10-09 21:40:16 +0300

    *

--- assets/general.js
+++ assets/general.js
@@ -6,10 +6,19 @@ const inputDisciplines = document.querySelector('#disciplines')
 const inputPractices = document.querySelector('#practices')
 const inputWork = document.querySelector('#self-work')
 
-function getStudPlane(){
-  
-}
+const out = document.querySelector('.out')
 
+function getStudPlane(){
+  fetch('./php/poop.php')
+    .then(res => res.json())
+    .then(res=>{
+     res.forEach(result=>{
+       console.log(result)
+       out.innerHTML += `<span>${result.index_plan}</span>`
+     })
+    })
+}
+getStudPlane()
 btnSend.addEventListener('click', ()=>{
   fetch('./php/poop.php', {
     method:"POST",
--- php/poop.php
+++ php/poop.php
@@ -3,7 +3,17 @@
 
   $link = new mysqli($host, $user, $password, $database);
 
-  if ($_SERVER["REQUEST_METHOD"] === "POST"){
+  if ($_SERVER["REQUEST_METHOD"] === "GET"){
+        $sql = "SELECT * FROM study_plan";
+        $result = $link->query($sql);
+    
+        while($row = $result->fetch_assoc()){
+            $items[] = $row;
+    
+        }
+        echo json_encode($items);
+  }
+  elseif ($_SERVER["REQUEST_METHOD"] === "POST"){
     $_POST = json_decode(file_get_contents('php://input'), true);
     $sql = "INSERT INTO `study_plan`  VALUES(null,'','$_POST[index]', '$_POST[name]', '$_POST[all]', '$_POST[disciplines]', '$_POST[practices]', '$_POST[self_work]')";
     $result = $link->query($sql);
